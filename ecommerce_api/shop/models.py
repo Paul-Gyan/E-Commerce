2b615d7b35bd442dffb1d1dcafa15465f449f461
PaulGyan <gyanpaulkwasi@gmail.com>
2024-10-17 01:49:35 +0000

    Added a new file

---
+++ ecommerce_api/shop/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
+    stock_quantity = models.IntegerField()
+    image_url = models.URLField(max_length=255)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    
