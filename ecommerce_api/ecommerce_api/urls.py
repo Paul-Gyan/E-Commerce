9274d133bbbc19754b0edc329346e5854afb6292
PaulGyan <gyanpaulkwasi@gmail.com>
2024-10-17 09:04:04 +0000

    Normalize all the line endings

--- ecommerce_api/ecommerce_api/urls.py
+++ ecommerce_api/ecommerce_api/urls.py
@@ -19,12 +19,15 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf.urls.static import static
 from django.conf import settings
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="E-Commerce API",
       default_version='v1',
-      description="Test description",
+      description="This is a test ecommerce api",
       terms_of_service="https://www.google.com/policies/terms/",
       contact=openapi.Contact(email="contact@snippets.local"),
       license=openapi.License(name="BSD License"),
